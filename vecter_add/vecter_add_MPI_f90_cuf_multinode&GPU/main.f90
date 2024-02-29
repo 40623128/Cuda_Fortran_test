92a7d0512ea262386ea3140b8e8cd66365bb98a5
40623128 <40623128@gm.nfu.edu.tw>
2024-02-29 15:27:04 +0800

    update vecter add multi_gpu_multi_node

--- vecter_add/vecter_add_MPI_f90_cuf_multinode&GPU/main.f90
+++ vecter_add/vecter_add_MPI_f90_cuf_multinode&GPU/main.f90
@@ -9,11 +9,14 @@ program main
 	type(dim3) :: blockSize, gridSize
 	real(8) :: sum_num,final_result
 	integer :: i
-	integer :: rank, num_procs ,gpu_rank ,num_gpu
+	integer :: rank, num_procs ,gpu_rank ,num_gpus
 	integer :: n, host_n, gpu_n
-	integer :: remainder
+	integer :: remainder,remainder_gpu
 	integer :: istat, ierr, GPU
+	integer, dimension(:),allocatable :: gpu_each_n
 	
+	
+	integer, dimension(:),allocatable :: gpu_devices
 	! Host input vectors
 	real(8),dimension(:,:),allocatable :: h_a
 	real(8),dimension(:,:),allocatable :: h_b
@@ -21,29 +24,30 @@ program main
 	real(8),dimension(:,:),allocatable :: h_c
 	
 	! Device input vectors
-	real(8),device,dimension(:,:),allocatable :: d_a
-	real(8),device,dimension(:,:),allocatable :: d_b
+	real(8),device,dimension(:),allocatable :: d_a
+	real(8),device,dimension(:),allocatable :: d_b
 	!Host output vector
-	real(8),device,dimension(:,:),allocatable :: d_c
+	real(8),device,dimension(:),allocatable :: d_c
+	
+	type(cudadeviceprop):: prop
+	
 	
 	! MPI Initialize
 	call mpiinit(rank, num_procs)
 	print *, 'rank: ', rank+1, '/', num_procs
 	! GPU Initialize
-	call gpu_init(num_gpu)
+	call gpu_init(num_gpus)
+	allocate(gpu_devices(num_gpus))
+	allocate(gpu_each_n(num_gpus))
 	! Size of vectors
-	do GPU = 0,7
-		istat=cudaSetDevice(GPU)
-	end do
+	n = 3200000
+	gpu_n = n / num_gpus
 	
-	n = 32000
-	
-	if (mod(n,num_procs) .eq. 0) then
-		remainder = mod(n,num_procs)
+	remainder = mod(n,num_procs)
+	if (remainder .eq. 0) then
 		host_n = n/num_procs
 		print *, 'host_n = ', host_n
 	else
-		remainder = mod(n,num_procs)
 		if (rank < remainder) then
 			host_n = floor(real(n)/real(num_procs))+1.0d0
 		else
@@ -52,93 +56,108 @@ program main
 		print *, 'rank = ', rank,',host_n = ', host_n
 	end if
 	
-	if (mod(n,num_procs) .eq. 0) then
-		remainder = mod(n,num_procs)
-		gpu_n = host_n/num_procs
-		print *, 'host_n = ', host_n
+	
+	
+	remainder_gpu = mod(host_n,num_gpus)
+	print *, 'remainder_gpu = ',remainder_gpu
+	if (remainder_gpu .eq. 0) then
+		gpu_n = host_n/num_gpus
+		print *, 'gpu_n = ', gpu_n
 	else
-		remainder = mod(n,num_procs)
-		if (rank < remainder) then
-			host_n = floor(real(n)/real(num_procs))+1.0d0
-		else
-			host_n = floor(real(n)/real(num_procs))
-		end if
-		print *, 'rank = ', rank,',host_n = ', host_n
+		gpu_n = host_n/num_gpus +1
+		do i = 1,num_gpus
+			if (i .le. remainder_gpu) then
+				gpu_each_n(i) = real(host_n/num_gpus)+1
+			else
+				gpu_each_n(i) = real(host_n/num_gpus)
+			end if
+		end do
+		print *, 'gpu_each_n = ', gpu_each_n
+		print *, 'gpu_n = ', gpu_n
 	end if
 	
+	do i = 1, num_gpus
+        gpu_devices(i) = i - 1
+    end do
+
 	! Allocate memory for each vector on host
-	allocate(h_a(num_gpu, host_n/num_gpu))
-	allocate(h_b(num_gpu, host_n/num_gpu))
-	allocate(h_c(num_gpu, host_n/num_gpu))
+	allocate(h_a(num_gpus, gpu_n))
+	allocate(h_b(num_gpus, gpu_n))
+	allocate(h_c(num_gpus, gpu_n))
 	
-	! Allocate memory for each vector on GPU
-	allocate(d_a(num_gpu, host_n/num_gpu))
-	allocate(d_b(num_gpu, host_n/num_gpu))
-	allocate(d_c(num_gpu, host_n/num_gpu))
-	
-	
-	if (remainder == 0) then
-		print *, 'remainder=', remainder, 'rank=', rank, 'host_n=', host_n
-		do i=1,host_n/num_gpu
-			do GPU = 0,7
-				h_a(GPU+1,i) = sin((i+(rank*num_gpu+GPU)*host_n/num_gpu)*1D0)**2
-				h_b(GPU+1,i) = cos((i+(rank*num_gpu+GPU)*host_n/num_gpu)*1D0)**2
+	!if (remainder_gpu .eq. 0) then
+		do i=1,gpu_n
+			do GPU = 1, num_gpus
+				h_a(GPU,i) = sin((gpu_n*GPU)+i*1D0)**2
+				h_b(GPU,i) = cos((gpu_n*GPU)+i*1D0)**2
 			end do
 		end do
 	!else
-	!	print *, 'remainder=', remainder, 'rank=', rank, 'host_n=', host_n
-	!	if (rank <remainder) then
-	!		do i=1,host_n
-	!			h_a(i) = sin((i+rank*host_n)*1D0)**2
-	!			h_b(i) = cos((i+rank*host_n)*1D0)**2
-	!		end do
-	!	else
-	!		do i=1,host_n
-	!			h_a(i) = sin((i+(rank-remainder)*host_n+remainder*(host_n+1))*1D0)**2
-	!			h_b(i) = cos((i+(rank-remainder)*host_n+remainder*(host_n+1))*1D0)**2
-	!		end do
-	!	end if
-	end if
+	!	do GPU = 1, num_gpus
+	!		if (GPU .le. remainder_gpu) then
+	!			do i=1,gpu_n
+	!				h_a(GPU,i) = sin((gpu_n*GPU)+i*1D0)**2
+	!				h_b(GPU,i) = cos((gpu_n*GPU)+i*1D0)**2
+	!			end do
+	!		else
+	!			do i=1,gpu_n-1
+	!				h_a(GPU,i) = sin((gpu_n*GPU)+i*1D0)**2
+	!				h_b(GPU,i) = cos((gpu_n*GPU)+i*1D0)**2
+	!			end do
+	!			h_a(GPU,gpu_n) = 0
+	!			h_b(GPU,gpu_n) = cos((gpu_n*GPU)+i*1D0)**2
+	!		end if
+	!	end do
+	!end if
 	print *, 'h_a h_b'
 	
-	d_a = h_a
-	d_b = h_b
-	print *, 'd_a(GPU+1,:) = h_a(GPU+1,:)'
-	ierr = cudaDeviceSynchronize()
+	do GPU = 1, num_gpus
+		print *, 'gpu_devices = ',gpu_devices(GPU)
+		istat=cudaSetDevice(gpu_devices(GPU))
+		istat=cudaGetDeviceProperties(prop,gpu_devices(GPU))
+		print *, 'device name = ',trim(prop%name)
+	end do
 	
-	! Number of threads in each thread block
-	blockSize = dim3(1024,1,1)
-	! Number of thread blocks in grid
-	gridSize = dim3(ceiling(real(gpu_n)/real(blockSize%x)) ,1,1)
-	print *, 'gridSize'
-	! Execute the kernel
-	do GPU = 0,0
-		istat=cudaSetDevice(GPU)
-		print *, 'cudaSetDevice'
+	do GPU = 1, num_gpus
+		istat=cudaSetDevice(gpu_devices(GPU))
+		allocate(d_a(gpu_n))
+		allocate(d_b(gpu_n))
+		allocate(d_c(gpu_n))
+		
+		istat=cudaMemcpy(d_a, h_a(GPU,:), gpu_n, cudaMemcpyHostToDevice)
+		istat=cudaMemcpy(d_b, h_b(GPU,:), gpu_n, cudaMemcpyHostToDevice)
+		
+		blockSize = dim3(1024,1,1)
+		gridSize = dim3(ceiling(real(gpu_n)/real(blockSize%x)) ,1,1)
 		call vecAdd_kernel<<<gridSize, blockSize>>>(gpu_n, d_a, d_b, d_c)
 	end do
-	print *, 'vecAdd_kernel'
-	ierr = cudaDeviceSynchronize()
 	
-    ! Implicit copy of device array to host
-	do GPU = 0,7
-		!istat=cudaSetDevice(GPU)
-		h_c(GPU,:) = d_c(GPU,:)
+	do GPU = 1, num_gpus
+		istat=cudaSetDevice(gpu_devices(GPU))
+		istat=cudaMemcpy(h_c(GPU,:), d_c, gpu_n, cudaMemcpyDeviceToHost)
 	end do
-    ierr = cudaDeviceSynchronize()
-	print *, 'vecAdd_kernel'
-    ! Sum up vector c and print result divided by n, this should equal 1 within error
-    sum_num = 0.0;
-    do i=1,host_n
-		do GPU = 0,7
-			sum_num = sum_num +  h_c(GPU+1,i)
+	
+	if (remainder_gpu .ne. 0) then
+		do GPU = 1, num_gpus
+			if (GPU .gt. remainder_gpu) then
+				h_c(GPU,gpu_n) = 0
+			end if
 		end do
-    end do
+	end if
+	
+	
+	
+	sum_num = 0.0;
+	do GPU = 1, num_gpus
+		do i = 1, gpu_n
+			sum_num = sum_num + h_c(GPU,i)
+		end do
+	end do
 	
     call MPI_REDUCE(sum_num, final_result, 1, MPI_DOUBLE_PRECISION, MPI_SUM, 0, MPI_COMM_WORLD, ierr)
 	
 	if (rank == 0) then
-		print *, 'final result: ', final_result
+		print *, 'final result: ', final_result/real(n)
 	end if
  
     ! Release device memory
