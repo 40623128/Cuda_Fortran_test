780197566c888abae076fced8922d856bda760ad
40623128 <40623128@gm.nfu.edu.tw>
2024-04-15 09:54:44 +0800

    update Q1 to 4D

--- quest/Q1/main.f90
+++ quest/Q1/main.f90
@@ -30,9 +30,10 @@ program main
 
 	type(dim3) :: blockSize, gridSize
 	real(8) :: sum_num
-	integer :: i, j, k
+	integer :: i, j, k,l
 	
 	! Size of vectors
+	integer :: total_cube
 	integer :: n, n_outside, total_n
 	integer :: n_x, n_x_total
 	integer :: n_y, n_y_total
@@ -41,9 +42,13 @@ program main
 	integer :: istat, ierr, GPU
 	integer :: num_gpus,devnum
 	! Host input vectors
-	real(8),dimension(:,:,:),allocatable :: all_mesh_1
-	real(8),dimension(:,:,:),allocatable :: all_mesh_2
-	real(8),dimension(:,:,:),allocatable :: all_mesh_3
+	!real(8),dimension(:,:,:),allocatable :: all_mesh_1
+	!real(8),dimension(:,:,:),allocatable :: all_mesh_2
+	!real(8),dimension(:,:,:),allocatable :: all_mesh_3
+	
+	real(8),dimension(:,:,:,:),allocatable :: all_mesh_1
+	real(8),dimension(:,:,:,:),allocatable :: all_mesh_2
+	real(8),dimension(:,:,:,:),allocatable :: all_mesh_3
 	
 	! Device input vectors
 	integer, dimension(:),allocatable :: gpu_devices
@@ -60,16 +65,17 @@ program main
 	call gpu_init(num_gpus)
 	num_gpus = 1
 	
-	n_x = 126
-	n_y = 126
-	n_z = 126
+	n_x = 16
+	n_y = 16
+	n_z = 16
 	n = n_x * n_y * n_z
 	
-	n_outside = 2
+	n_outside = 1
 	n_x_total = n_x + 2*n_outside
 	n_y_total = n_y + 2*n_outside
 	n_z_total = n_z + 2*n_outside
 	
+	total_cube = 1960
 	total_n = n_x_total * n_y_total * n_z_total
 	
 	allocate(gpu_devices(num_gpus))
@@ -78,23 +84,25 @@ program main
         gpu_devices(GPU) = GPU - 1
     end do
 	! Allocate memory for all mesh on host
-	allocate(all_mesh_1(n_x_total, n_y_total, n_z_total))
-	allocate(all_mesh_2(n_x_total, n_y_total, n_z_total))
-	allocate(all_mesh_3(n_x_total, n_y_total, n_z_total))
+	allocate(all_mesh_1(total_cube, n_x_total, n_y_total, n_z_total))
+	allocate(all_mesh_2(total_cube, n_x_total, n_y_total, n_z_total))
+	allocate(all_mesh_3(total_cube, n_x_total, n_y_total, n_z_total))
 	
 	print*, 'allocate host memory completed'
 	
 	
 	!Initialize content of mesh, mesh(x,y,z) = 1
-	do i = 1, n_x_total
-		do j = 1, n_x_total
-			do k = 1, n_x_total
-				all_mesh_1(i,j,k) = 1.d0
-				all_mesh_2(i,j,k) = 0.d0
-				all_mesh_3(i,j,k) = 0.d0
+	do l = 1, total_cube
+		do i = 1, n_x_total
+			do j = 1, n_x_total
+				do k = 1, n_x_total
+					all_mesh_1(l,i,j,k) = i+j+k
+					all_mesh_2(l,i,j,k) = 0.d0
+					all_mesh_3(l,i,j,k) = 0.d0
+				end do
 			end do
 		end do
-    end do
+	end do
 	print*, 'Initialize content of mesh completed'
 	
 	!check device
@@ -110,24 +118,18 @@ program main
 		istat=cudaSetDevice(gpu_devices(GPU))
 		allocate(d_mesh_1(n_x_total, n_y_total, n_z_total))
 		allocate(d_mesh_2(n_x_total, n_y_total, n_z_total))
-		istat=cudaMemcpy(d_mesh_1, all_mesh_1, total_n, cudaMemcpyHostToDevice)
-		istat=cudaMemcpy(d_mesh_2, all_mesh_2, total_n, cudaMemcpyHostToDevice)
-		blockSize = dim3(8,8,8)
-		gridSize = dim3(ceiling(real(n_x_total)/real(blockSize%x)) ,ceiling(real(n_y_total)/real(blockSize%y)),ceiling(real(n_z_total)/real(blockSize%z)))
-		!istat = cudaEventSynchronize(event)
-		call meshAdd_3Dkernel<<<gridSize, blockSize>>>(d_mesh_1, n_x_total, n_y_total, n_z_total, n_outside, d_mesh_2)
-		!call meshAdd2Tier_3Dkernel<<<gridSize, blockSize>>>(d_mesh_1, n_x_total, n_y_total, n_z_total, n_outside, d_mesh_2)
+		do l = 1, total_cube
+			istat=cudaMemcpy(d_mesh_1, all_mesh_1(l,:,:,:), total_n, cudaMemcpyHostToDevice)
+			istat=cudaMemcpy(d_mesh_2, all_mesh_2(l,:,:,:), total_n, cudaMemcpyHostToDevice)
+			blockSize = dim3(8,8,8)
+			gridSize = dim3(ceiling(real(n_x_total)/real(blockSize%x)) ,ceiling(real(n_y_total)/real(blockSize%y)),ceiling(real(n_z_total)/real(blockSize%z)))
+			!istat = cudaEventSynchronize(event)
+			call meshAdd_3Dkernel<<<gridSize, blockSize>>>(d_mesh_1, n_x_total, n_y_total, n_z_total, n_outside, d_mesh_2)
+			!call meshAdd2Tier_3Dkernel<<<gridSize, blockSize>>>(d_mesh_1, n_x_total, n_y_total, n_z_total, n_outside, d_mesh_2)
+			istat=cudaMemcpy(all_mesh_2(l,:,:,:), d_mesh_2, total_n, cudaMemcpyDeviceToHost)
+		end do
 	end do
-	do GPU = 1, num_gpus
-		istat = cudaSetDevice(gpu_devices(GPU))
-		istat = cudaDeviceSynchronize()
-	end do
-	print*,'calculate completed'
 	
-	do GPU = 1, num_gpus
-		istat=cudaSetDevice(gpu_devices(GPU))
-		istat=cudaMemcpy(all_mesh_2, d_mesh_2, total_n, cudaMemcpyDeviceToHost)
-	end do
 	do GPU = 1, num_gpus
 		istat=cudaSetDevice(gpu_devices(GPU))
 		istat = cudaDeviceSynchronize()
@@ -135,28 +137,31 @@ program main
 	print*,'copy data of device to host completed'
 	
 	print*,'CPU Calculating '
-	do i = 2, n_x_total-1
-		do j = 2, n_x_total-1
-			do k = 2, n_x_total-1
-				all_mesh_3(i,j,k) = all_mesh_1(i+1, j, k) + all_mesh_1(i, j+1, k) + all_mesh_1(i+1, j+1, k)&
-									+ all_mesh_1(i-1, j, k) + all_mesh_1(i, j-1, k) + all_mesh_1(i, j, k-1)
+	do l = 1, total_cube
+		do i = 1+n_outside, n_x_total-n_outside
+			do j = 1+n_outside, n_x_total-n_outside
+				do k = 1+n_outside, n_x_total-n_outside
+					all_mesh_3(l,i,j,k) = all_mesh_1(l, i+1, j, k) + all_mesh_1(l, i, j+1, k) + all_mesh_1(l, i, j, k+1)&
+										+ all_mesh_1(l, i-1, j, k) + all_mesh_1(l, i, j-1, k) + all_mesh_1(l, i, j, k-1)
+				end do
 			end do
 		end do
-    end do
+	end do
 	print*,'CPU Calculate Completed'
 	
-	
-	do i = 3, n_x_total-2
-		do j = 3, n_x_total-2
-			do k = 3, n_x_total-2
-				if (all_mesh_2(i, j, k) /= all_mesh_3(i, j, k)) then
-					print*,'i =',i, 'j =',j,'k =',k
-				end if
+	do l = 1, total_cube
+		do i = 1+n_outside, n_x_total-n_outside
+			do j = 1+n_outside, n_x_total-n_outside
+				do k = 1+n_outside, n_x_total-n_outside
+					if (all_mesh_2(l,i, j, k) /= all_mesh_3(l,i, j, k)) then
+						print*,'i =',i, 'j =',j,'k =',k
+					end if
+				end do
 			end do
 		end do
-    end do
-	print*,all_mesh_2(20,20,20)
-	print*,all_mesh_3(20,20,20)
+	end do
+	print*,all_mesh_2(1, 8, 8, 8)
+	print*,all_mesh_3(1, 8, 8, 8)
  
  
     print *, 'end '
